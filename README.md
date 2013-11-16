20bdd656b60b77474b99b5ec970a25f72970773c
Brent Coney <brentconey@gmail.com>
2013-11-16 11:16:42 -0600

    Initial Commit of simple modal

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 simple-modal
 ============
 
-This is a simple js modal library.
+This is a super simple, out of your way, js modal library.
+Dependencies: jQuery
+
+To use: First, you need to add the overlay to the page. The overlay can be anywhere and only needs to be on the page once. <div class="modal-overlay"></div>
+		Add a class of Modal to the div you would like to be a modal.
+			Ex. <div class="modal" id="add_name"><!-- any html here --></div>
+			
+		Add a modal activator that will show the modal.
+			Ex. <a class="modal-activator" href="#add_name">Show Modal</a>
+			
+		Now, you need to add the js and css to use simplemodal. Simplemodal has a dependency on jQuery so make sure it's in your project.
+			<link rel="stylesheet" media="all" href="simplemodal.css" />
+			<script type="text/javascript" src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+			<script type="text/javascript" src="simplemodal.js"></script>
+			
+		Once you have jQuery and the simplemodal.js files loaded, all you need to do is call simplemodal on your div.
+			Ex. $("#add_name").simplemodal();
+			This is the most basic usage.
+		
+		If your modal div has a form, simple modal will read the action and method of the form and set up an ajax call passing all the input data within the div automatically.
+		
+		Options: Simplemodal has three properties that you can use.
+				 Show: This defaults to false and can be overridden to true to show the modal on page load.
+					   Ex. $("#add_name").simplemodal({ show: true });
+				 ajaxDone: This is a hook into the successful completion of the ajax call.
+					   Ex. $("#add_name").simplemodal({ ajaxDone: successFunction });
+							function successFunction(data){
+								//this will fire when the ajax even is done, passing back any data from the call
+							}
+				 ajaxError: This is the same as ajaxDone except that it fires when an error with the ajax call occurs.
+					   Ex. $("#add_name").simplemodal({ ajaxError: errorHanlder });
+							function errorHandler(data){
+								//error handling code
+							}
+
+Contributors: Brent Coney & Jordan Little
---
+++ simplemodal.js
@@ -0,0 +1,91 @@
+﻿(function ($) {
+    $.fn.simplemodal = function (options) {
+        var $modal = $(this);
+        var defaultOptions = $.extend({
+            show: false,
+            ajaxDone: null,
+            ajaxError: null
+        }, options);
+
+        if (defaultOptions.show) {
+            showModal();
+        } else {
+            hideModal();
+        }
+        var $modalForm = $modal.find("form");
+
+        if ($modalForm.length > 0) {
+            //we have a form in the modal
+            //attach events and figure out data
+            var action = $modalForm.attr("action");
+            var method = $modalForm.attr("method");
+            var $formInputs = $modalForm.find('input, textarea, select')
+                .not(':input[type=button], :input[type=submit], :input[type=reset]');
+            
+            var $submitButton = $($modalForm.find("input[type=submit]")[0]);
+
+            $(document).on("click", '#' + $submitButton.attr("id"), function (e) {
+                e.preventDefault();
+                //do the ajax stuff to send the data
+                //then fire the submitFunction
+                var dataObjectToSend = {};
+                $.each($formInputs, function (index, input) {
+                    var inputName = $(input).attr("name");
+                    //set up empty object to fill
+                    dataObjectToSend[inputName] = $(input).val();
+                });
+
+                $.ajax({
+                    type: method,
+                    url: action,
+                    data: dataObjectToSend
+                }).done(function (data) {
+                    $.each($formInputs, function (index, input) {
+                        $(input).val('');
+                    });
+                    hideModal();
+                    if (defaultOptions.ajaxDone) {
+                        defaultOptions.ajaxDone(data);
+                    }
+                }).fail(function () {
+                    hideModal();
+                    if (defaultOptions.ajaxError) {
+                        defaultOptions.ajaxError();
+                    }
+                });
+                
+            });
+        }
+        var modalActivator = $modal.attr("id");
+        $(document).on('click','a[href="#' + modalActivator + '"]', function (e) {
+            e.preventDefault();
+            showModal();
+            $modal.find("input").first().focus(); 
+        });
+
+        function showModal() {
+            $(".modal-overlay").addClass("modal-overlay--show");
+            $modal.addClass("modal--show");
+        }
+        
+    };
+    //global click handlers
+    $(document).on("keyup", function (e) {
+        if (e.keyCode === 27) {
+            $(".modal-overlay").removeClass("modal-overlay--show");
+            $(".modal").removeClass("modal--show");
+        }
+    });
+
+    $(document).on("click", ".modal-overlay, .modal-close", function () {
+        hideModal();
+    });
+
+    function hideModal() {
+        $('.modal').removeClass("modal--show");
+        $(".modal-overlay").removeClass("modal-overlay--show");
+    }
+    
+
+    
+})(window.jQuery);
